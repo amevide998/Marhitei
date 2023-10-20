6b049b86bbce034bac16173a09fd372df1a4cac8
amevide998 <hdsvidi@gmail.com>
2023-10-20 17:12:14 +0700

    initial project

---
+++ src/main/java/com/example/marhitei/MarhiteiApplication.java
@@ -0,0 +1,13 @@
+package com.example.marhitei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarhiteiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarhiteiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marhitei/MarhiteiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marhitei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarhiteiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
